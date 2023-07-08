05e2a186e830136be1faec92f8ce5246b51add62
hamzeh-pourshabanan <h.pourshabanan89@gmail.com>
2023-07-08 15:51:29 +0330

    feat: Create viewmodel factory

--- app/src/main/java/com/example/myapplication/ui/SearchUsersActivity.kt
+++ app/src/main/java/com/example/myapplication/ui/SearchUsersActivity.kt
@@ -1,8 +1,38 @@
 package com.example.myapplication.ui
 
+import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.DividerItemDecoration
+import com.example.myapplication.Injection
+import com.example.myapplication.databinding.ActivitySearchRepositoriesBinding
 
 class SearchUsersActivity: AppCompatActivity() {
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val binding = ActivitySearchRepositoriesBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
 
+        // get the view model
+//        val viewModel = ViewModelProvider(
+//            this, Injection.provideViewModelFactory(
+//                context = this,
+//                owner = this
+//            )
+//        )
+//            .get(SearchRepositoriesViewModel::class.java)
+//
+//        // add dividers between RecyclerView's row items
+//        val decoration = DividerItemDecoration(this, DividerItemDecoration.VERTICAL)
+//        binding.list.addItemDecoration(decoration)
+//
+//        // bind the state
+//        binding.bindState(
+//            uiState = viewModel.state,
+//            pagingData = viewModel.pagingDataFlow,
+//            uiActions = viewModel.accept
+//        )
+    }
 }
---
+++ app/src/main/java/com/example/myapplication/ui/ViewModelFactory.kt
@@ -0,0 +1,28 @@
+package com.example.myapplication.ui
+
+import androidx.lifecycle.AbstractSavedStateViewModelFactory
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.savedstate.SavedStateRegistryOwner
+import com.example.myapplication.data.GithubRepository
+
+/**
+ * Factory for ViewModels
+ */
+class ViewModelFactory(
+    owner: SavedStateRegistryOwner,
+    private val repository: GithubRepository
+) : AbstractSavedStateViewModelFactory(owner, null) {
+
+    override fun <T : ViewModel?> create(
+        key: String,
+        modelClass: Class<T>,
+        handle: SavedStateHandle
+    ): T {
+        if (modelClass.isAssignableFrom(SearchUsersViewModel::class.java)) {
+            @Suppress("UNCHECKED_CAST")
+            return SearchUsersViewModel(repository, handle) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class")
+    }
+}
