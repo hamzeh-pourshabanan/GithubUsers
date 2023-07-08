33d4fa1c78bb1cad4a2306aea1f22b49832476d6
hamzeh-pourshabanan <h.pourshabanan89@gmail.com>
2023-07-08 14:24:57 +0330

    feat: Create db singleton

--- app/src/main/java/com/example/myapplication/db/UserDatabase.kt
+++ app/src/main/java/com/example/myapplication/db/UserDatabase.kt
@@ -1,6 +1,8 @@
 package com.example.myapplication.db
 
+import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
 import com.example.myapplication.model.User
 
@@ -12,4 +14,23 @@ import com.example.myapplication.model.User
 abstract class UserDatabase: RoomDatabase() {
 
     abstract fun usersDao(): UsersDao
+
+    companion object {
+
+        @Volatile
+        private var INSTANCE: UserDatabase? = null
+
+        fun getInstance(context: Context): UserDatabase =
+            INSTANCE ?: synchronized(this) {
+                INSTANCE
+                    ?: buildDatabase(context).also { INSTANCE = it }
+            }
+
+        private fun buildDatabase(context: Context) =
+            Room.databaseBuilder(
+                context.applicationContext,
+                UserDatabase::class.java, "Github.db"
+            )
+                .build()
+    }
 }
