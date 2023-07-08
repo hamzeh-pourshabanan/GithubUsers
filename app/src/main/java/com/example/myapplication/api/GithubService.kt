4204f40ae3d7122f075473fe390d746bb9d71e5b
hamzeh-pourshabanan <h.pourshabanan89@gmail.com>
2023-07-08 14:12:20 +0330

    feat: Create retrofit service

---
+++ app/src/main/java/com/example/myapplication/api/GithubService.kt
@@ -0,0 +1,45 @@
+package com.example.myapplication.api
+
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+const val IN_QUALIFIER = "in:login"
+
+/**
+ * Github API communication setup via Retrofit.
+ */
+interface GithubService {
+
+    /**
+     * Get users.
+     */
+    @GET("search/users?sort=stars")
+    suspend fun searchUsers(
+        @Query("q") query: String,
+        @Query("page") page: Int,
+        @Query("per_page") itemsPerPage: Int
+    ): UserSearchResponse
+
+    companion object {
+        private const val BASE_URL = "https://api.github.com/"
+
+        fun create(): GithubService {
+            val logger = HttpLoggingInterceptor()
+            logger.level = HttpLoggingInterceptor.Level.BASIC
+
+            val client = OkHttpClient.Builder()
+                .addInterceptor(logger)
+                .build()
+            return Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .client(client)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+                .create(GithubService::class.java)
+        }
+    }
+}
