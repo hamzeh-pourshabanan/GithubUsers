04585a9689891b6b39287a4be827231c067be058
hamzeh-pourshabanan <h.pourshabanan89@gmail.com>
2023-07-08 15:04:14 +0330

    feat: Add resource drawables

---
+++ app/src/main/java/com/example/myapplication/ui/UserViewHolder.kt
@@ -0,0 +1,10 @@
+package com.example.myapplication.ui
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+
+/**
+ * View Holder for a [User] RecyclerView list item.
+ */
+class UserViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+}
