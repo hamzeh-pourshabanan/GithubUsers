1a37adaaed26431318bf5a69ed53fe078af241ed
hamzeh-pourshabanan <h.pourshabanan89@gmail.com>
2023-07-08 14:34:13 +0330

    feat: Create repository

---
+++ app/src/main/java/com/example/myapplication/data/GithubRepository.kt
@@ -0,0 +1,47 @@
+package com.example.myapplication.data
+
+import android.util.Log
+import androidx.paging.ExperimentalPagingApi
+import androidx.paging.Pager
+import androidx.paging.PagingConfig
+import androidx.paging.PagingData
+import com.example.myapplication.api.GithubService
+import com.example.myapplication.db.UserDatabase
+import com.example.myapplication.model.User
+import kotlinx.coroutines.flow.Flow
+
+/**
+ * Repository class that works with local and remote data sources.
+ */
+class GithubRepository(
+    private val service: GithubService,
+    private val database: UserDatabase
+) {
+
+    /**
+     * Search repositories whose names match the query, exposed as a stream of data that will emit
+     * every time we get more data from the network.
+     */
+    fun getSearchResultStream(query: String): Flow<PagingData<User>> {
+        Log.d("GithubRepository", "New query: $query")
+
+        // appending '%' so we can allow other characters to be before and after the query string
+        val dbQuery = "%${query.replace(' ', '%')}%"
+        val pagingSourceFactory = { database.usersDao().usersByName(dbQuery) }
+
+        @OptIn(ExperimentalPagingApi::class)
+        return Pager(
+            config = PagingConfig(pageSize = NETWORK_PAGE_SIZE, enablePlaceholders = false),
+            remoteMediator = GithubRemoteMediator(
+                query,
+                service,
+                database
+            ),
+            pagingSourceFactory = pagingSourceFactory
+        ).flow
+    }
+
+    companion object {
+        const val NETWORK_PAGE_SIZE = 30
+    }
+}
